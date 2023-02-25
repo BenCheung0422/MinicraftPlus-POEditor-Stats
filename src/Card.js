6a7133ee857165f03effe4685f58f13a58fb0af5
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-02-25 13:48:56 +0800

    Rewrite the framework from Next.js

---
+++ src/Card.js
@@ -0,0 +1,230 @@
+// @ts-check
+
+import { getAnimations } from "./getStyles.js";
+import { encodeHTML, flexLayout } from "./utils.js";
+
+class Card {
+  /** @type {number} */
+  width;
+  /** @type {number} */
+  height;
+  /** @type {boolean} */
+  hideBorder;
+  /** @type {boolean} */
+  hideTitle;
+  /** @type {number} */
+  border_radius;
+  /** @type {{[k in "titleColor"|"textColor"|"bgColor"|"borderColor"]?: string|string[]}} */
+  colors;
+  /** @type {string} */
+  title;
+  /** @type {string} */
+  css;
+  /** @type {number} */
+  paddingX;
+  /** @type {number} */
+  paddingY;
+  /** @type {boolean} */
+  animations;
+  /** @type {string} */
+  a11yTitle;
+  /** @type {string} */
+  a11yDesc;
+
+  /**
+   * Creates a new card instance.
+   *
+   * @param {object} args Card arguments.
+   * @param {number=} args.width Card width.
+   * @param {number=} args.height Card height.
+   * @param {number=} args.border_radius Card border radius.
+   * @param {typeof this.colors=} args.colors Card border radius.
+   * @param {string} args.title Card custom title.
+   */
+  constructor({
+    width = 100,
+    height = 100,
+    border_radius = 4.5,
+    colors = {},
+    title,
+  }) {
+    this.width = width;
+    this.height = height;
+
+    this.hideBorder = false;
+    this.hideTitle = false;
+
+    this.border_radius = border_radius;
+
+    // returns theme based colors with proper overrides and defaults
+    this.colors = colors;
+    this.title = encodeHTML(title);
+
+    this.css = "";
+
+    this.paddingX = 25;
+    this.paddingY = 35;
+    this.animations = true;
+    this.a11yTitle = "";
+    this.a11yDesc = "";
+  }
+
+  disableAnimations() {
+    this.animations = false;
+  }
+
+  /**
+   * @param {{title: string, desc: string}} prop
+   */
+  setAccessibilityLabel({ title, desc }) {
+    this.a11yTitle = title;
+    this.a11yDesc = desc;
+  }
+
+  /**
+   * @param {string} value
+   */
+  setCSS(value) {
+    this.css = value;
+  }
+
+  /**
+   * @param {boolean} value
+   */
+  setHideBorder(value) {
+    this.hideBorder = value;
+  }
+
+  /**
+   * @param {boolean} value
+   */
+  setHideTitle(value) {
+    this.hideTitle = value;
+    if (value) {
+      this.height -= 30;
+    }
+  }
+
+  /**
+   * @param {string} text
+   */
+  setTitle(text) {
+    this.title = text;
+  }
+
+  renderTitle() {
+    const titleText = `
+      <text
+        x="0"
+        y="0"
+        class="header"
+        data-testid="header"
+      >${this.title}</text>
+    `;
+
+    return `
+      <g
+        data-testid="card-title"
+        transform="translate(${this.paddingX}, ${this.paddingY})"
+      >
+        ${flexLayout({
+          items: [titleText],
+          gap: 25,
+        }).join("")}
+      </g>
+    `;
+  }
+
+  renderGradient() {
+    if (typeof this.colors.bgColor !== "object") return "";
+
+    const gradients = this.colors.bgColor.slice(1);
+    return typeof this.colors.bgColor === "object"
+      ? `
+        <defs>
+          <linearGradient
+            id="gradient"
+            gradientTransform="rotate(${this.colors.bgColor[0]})"
+            gradientUnits="userSpaceOnUse"
+          >
+            ${gradients.map((grad, index) => {
+              let offset = (index * 100) / (gradients.length - 1);
+              return `<stop offset="${offset}%" stop-color="#${grad}" />`;
+            })}
+          </linearGradient>
+        </defs>
+        `
+      : "";
+  }
+
+  /**
+   * @param {string} body
+   */
+  render(body) {
+    return `
+      <svg
+        width="${this.width}"
+        height="${this.height}"
+        viewBox="0 0 ${this.width} ${this.height}"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-labelledby="descId"
+      >
+        <title id="titleId">${this.a11yTitle}</title>
+        <desc id="descId">${this.a11yDesc}</desc>
+        <style>
+          .header {
+            font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
+            fill: ${this.colors.titleColor};
+            animation: fadeInAnimation 0.8s ease-in-out forwards;
+          }
+          @supports(-moz-appearance: auto) {
+            /* Selector detects Firefox */
+            .header { font-size: 15.5px; }
+          }
+          ${this.css}
+
+          ${process.env.NODE_ENV === "test" ? "" : getAnimations()}
+          ${
+            this.animations === false
+              ? `* { animation-duration: 0s !important; animation-delay: 0s !important; }`
+              : ""
+          }
+        </style>
+
+        ${this.renderGradient()}
+
+        <rect
+          data-testid="card-bg"
+          x="0.5"
+          y="0.5"
+          rx="${this.border_radius}"
+          height="99%"
+          stroke="${this.colors.borderColor}"
+          width="${this.width - 1}"
+          fill="${
+            typeof this.colors.bgColor === "object"
+              ? "url(#gradient)"
+              : this.colors.bgColor
+          }"
+          stroke-opacity="${this.hideBorder ? 0 : 1}"
+        />
+
+        ${this.hideTitle ? "" : this.renderTitle()}
+
+        <g
+          data-testid="main-card-body"
+          transform="translate(0, ${
+            this.hideTitle ? this.paddingX : this.paddingY + 20
+          })"
+        >
+          ${body}
+        </g>
+      </svg>
+    `;
+  }
+}
+
+export { Card };
+export default Card;
---
+++ src/metadata.js
@@ -0,0 +1,232 @@
+// @ts-check
+
+import { Card } from "./Card.js";
+import { getStyles } from "./getStyles.js";
+import { flexLayout, getCardColors, measureText } from "./utils.js";
+
+const CARD_MIN_WIDTH = 287;
+const CARD_DEFAULT_WIDTH = 287;
+const RANK_CARD_MIN_WIDTH = 420;
+const RANK_CARD_DEFAULT_WIDTH = 450;
+
+export class Project {
+    /** @readonly @type {number} Integer */
+    id;
+    /** @readonly @type {string} */
+    name;
+    /** @readonly @type {string} */
+    description;
+    /** @readonly @type {number} Integer */
+    public;
+    /** @readonly @type {number} Integer */
+    open;
+    /** @readonly @type {string} */
+    reference_language;
+    /** @readonly @type {string} */
+    fallback_language;
+    /** @readonly @type {number} Integer */
+    terms;
+    /** @readonly @type {Date} */
+    created;
+    /** @param {object} obj The project object from response, this must be validated as valid. */
+    constructor(obj) {
+        this.id = obj.id;
+        this.name = obj.name;
+        this.description = obj.description;
+        this.public = obj.public;
+        this.open = obj.open;
+        this.reference_language = obj.reference_language;
+        this.fallback_language = obj.fallback_language;
+        this.terms = obj.terms;
+        this.created = new Date(obj.created);
+    }
+}
+
+export class Language {
+    /** @readonly @type {string} */
+    name;
+    /** @readonly @type {string} */
+    code;
+    /** @readonly @type {number} Integer */
+    translations;
+    /** @readonly @type {number} Decimal */
+    percentage;
+    /** @readonly @type {Date} */
+    updated;
+    /** @param {object} obj The language object from response, this must be validated as valid. */
+    constructor(obj) {
+        this.name = obj.name;
+        this.code = obj.code;
+        this.translations = obj.translations;
+        this.percentage = obj.percentage;
+        this.updated = new Date(obj.updated);
+    }
+}
+
+const LINE_HEIGHT = 10;
+
+/**
+ * Create a stats card text item.
+ * @param {object} createTextNodeParams Object that contains the createTextNode parameters.
+ * @param {string} createTextNodeParams.label The label to display.
+ * @param {string} createTextNodeParams.value The value to display.
+ * @param {number} createTextNodeParams.index The index of the stat.
+ * @param {number} createTextNodeParams.shiftValuePos Number of pixels the value has to be shifted to the right.
+ * @returns
+ */
+const createTextNode = ({label, value, index, shiftValuePos}) => {
+    const staggerDelay = (index + 3) * 150;
+    return `
+        <g class="stagger" style="animation-delay: ${staggerDelay}ms" transform="translate(25, 0)">
+            <text class="stat bold" y="12.5">${label}:</text>
+            <text
+                class="stat bold"
+                x="${120 + shiftValuePos}"
+                y="12.5"
+            >${value}</text>
+        </g>
+    `;
+}
+
+/**
+ * Rendering the stats card.
+ * @param {Project} project The project data.
+ * @param {Language[]} languages The language data.
+ * @returns {string} The stats card SVG object.
+ */
+export const renderStatsCard = (project, languages) => {
+    // returns theme based colors with proper overrides and defaults
+    const { titleColor, textColor, bgColor, borderColor, ringColor } =
+        getCardColors({
+            title_color: "",
+            text_color: "",
+            bg_color: "",
+            border_color: "",
+            ring_color: "",
+            theme: "default",
+        });
+
+    const STATS = {
+        reference_language: {
+            label: "Reference Language",
+            value: languages.find(lang => lang.code === project.reference_language)?.name ?? "N/A"
+        },
+        terms: {
+            label: "Terms",
+            value: project.terms.toLocaleString("en-US")
+        },
+        languages: {
+            label: "Languages",
+            value: languages.length.toLocaleString("en-US")
+        }
+    };
+
+    const statItems = Object.keys(STATS)
+        .map((key, index) =>
+            // create the text nodes, and pass index so that we can calculate the line spacing
+            createTextNode({
+                ...STATS[key],
+                index,
+                shiftValuePos: 79.01
+            }),
+        );
+
+    // Calculate the card height depending on how many items there are
+    // but if rank circle is visible clamp the minimum height to `150`
+    let height = Math.max(45 + (statItems.length + 1) * LINE_HEIGHT, 150);
+
+    const cssStyles = getStyles({
+        titleColor,
+        ringColor,
+        textColor,
+        progress: 0
+    });
+
+    const calculateTextWidth = () => {
+        return measureText(project.name);
+    };
+
+    let width = RANK_CARD_DEFAULT_WIDTH;
+    if (width < RANK_CARD_MIN_WIDTH) {
+        width = RANK_CARD_MIN_WIDTH;
+    }
+
+    const card = new Card({
+        title: project.name,
+        width,
+        height,
+        colors: {
+          titleColor,
+          textColor,
+          bgColor,
+          borderColor,
+        },
+    });
+
+    card.setHideBorder(false);
+    card.setHideTitle(false);
+    card.setCSS(cssStyles);
+
+    /**
+     * Calculates the right rank circle translation values such that the rank circle
+     * keeps respecting the following padding:
+     *
+     * width > RANK_CARD_DEFAULT_WIDTH: The default right padding of 70 px will be used.
+     * width < RANK_CARD_DEFAULT_WIDTH: The left and right padding will be enlarged
+     *   equally from a certain minimum at RANK_CARD_MIN_WIDTH.
+     *
+     * @returns {number} - Rank circle translation value.
+     */
+    const calculateRankXTranslation = () => {
+        const minXTranslation = RANK_CARD_MIN_WIDTH - 70;
+        if (width > RANK_CARD_DEFAULT_WIDTH) {
+            const xMaxExpansion = minXTranslation + (450 - RANK_CARD_MIN_WIDTH) / 2;
+            return xMaxExpansion + width - RANK_CARD_DEFAULT_WIDTH;
+        } else {
+            return minXTranslation + (width - RANK_CARD_MIN_WIDTH) / 2;
+        }
+    };
+
+    // // Conditionally rendered elements
+    // const rankCircle =
+    // `<g data-testid="rank-circle"
+    //     transform="translate(${calculateRankXTranslation()}, ${
+    //     height / 2 - 50
+    // })">
+    //     <circle class="rank-circle-rim" cx="-10" cy="8" r="40" />
+    //     <circle class="rank-circle" cx="-10" cy="8" r="40" />
+    //     <g class="rank-text">
+    //     <text
+    //         x="-5"
+    //         y="3"
+    //         alignment-baseline="central"
+    //         dominant-baseline="central"
+    //         text-anchor="middle"
+    //     >
+    //         ${rank.level}
+    //     </text>
+    //     </g>
+    // </g>`;
+
+    // Accessibility Labels
+    const labels = Object.keys(STATS)
+        .map((key) => `${STATS[key].label}: ${STATS[key].value}`)
+        .join(", ");
+
+    card.setAccessibilityLabel({
+        title: `${card.title}`,
+        desc: labels,
+    });
+
+    return card.render(`
+        <svg x="0" y="0">
+            ${flexLayout({
+                items: statItems,
+                gap: LINE_HEIGHT,
+                direction: "column",
+            }).join("")}
+        </svg>
+    `);
+}
+
+export default renderStatsCard;
